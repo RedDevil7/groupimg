3ab2764580d2d8fbe2e16c5804eab56881f1bfd3
rr- <mkurczew@gmail.com>
2013-05-12 12:45:17 +0200

    Cosmetic changes

--- hashes.sh
+++ hashes.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/bash
 ext="jpg;png;gif"
-find_expr=$(sed 's/;/ -or -iname *./g;s/^/-iname *./g' <<< $ext)
 filelist_prefix="filelist_"
 
-folder=$1
+folder="$1"
 if [ ! -d "$folder" ]; then
 	[ -n "$folder" ] && (echo Error: folder \""$folder"\" does not exist. >&2)
 	echo Usage: "$0" FOLDER >&2
 	exit 1
 fi
 
+find_expr=$(sed 's/;/ -or -iname *./g;s/^/-iname *./g' <<< $ext)
 find "$folder" -type f $find_expr|split -d - "$filelist_prefix"
 
 subscript='
-while read line; do
+while read path; do
 	hash=0
-	echo -e "$line\t$hash"
+	echo -e "$path\t$hash"
 done <$0
 '
 
